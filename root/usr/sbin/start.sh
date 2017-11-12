a42ff32fe5e5552734c5eaa5869c4776a55e87c2
Adrian Jones <adrianjones@Adrians-MacBook-Pro.local>
2017-11-12 00:36:48 +0000

    Added while loop to keep container up

--- root/usr/sbin/start.sh
+++ root/usr/sbin/start.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
+while true; do
+	#Check Variables
+	if [ -z ${PLAYLIST+x} ]; then
+        	echo "PLAYLIST is unset" 
+		exit
+ 	else
+        	echo "PLAYLIST is set to '$PLAYLIST'"
+	fi
 
-#Check Variables
-if [ -z ${PLAYLIST+x} ]; then
-        echo "PLAYLIST is unset" 
-	exit
- else
-        echo "PLAYLIST is set to '$PLAYLIST'"
-fi
 
+	#Download Playlist
+	echo "Dowloading $PLAYLIST"
+	youtube-dl $PLAYLIST -i \
+	-o "/persistent/%(title)s.%(ext)s" \
+	-f bestvideo+bestaudio \
+	--no-check-certificate \
+	--postprocessor-args "-strict experimental" \
+	--download-archive /persistent/archive
+	chmod -R 777 /persistent/
 
-#Download Playlist
-echo "Dowloading $PLAYLIST"
-youtube-dl $PLAYLIST -i \
--o "/persistent/%(title)s.%(ext)s" \
--f bestvideo+bestaudio \
---no-check-certificate \
---postprocessor-args "-strict experimental" \
---download-archive /persistent/archive
-chmod -R 777 /persistent/
+	#Create NFOs
+	if [ "$NFO" = "true" ]; then
+		echo "NFO creation enabled"
+		ruby /usr/sbin//make-mvid-nfo.rb /persistent/
+	fi
 
-#Create NFOs
-if [ "$NFO" = "true" ]; then
-	echo "NFO creation enabled"
-	ruby /usr/sbin//make-mvid-nfo.rb /persistent/
-fi
-
-echo "Done Sleeping for $INTERVAL seconds"
-sleep $INTERVAL
+	echo "Done Sleeping for $INTERVAL seconds"
+	sleep $INTERVAL
+done
